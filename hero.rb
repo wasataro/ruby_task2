f0abf98caa6368b884029a9b1fd8ff8731b6e000
阿部宏祐 <abekosuke@kosukeMBP.local>
2020-02-23 22:47:12 +0900

    課題２の提出

--- hero.rb
+++ hero.rb
@@ -1,5 +1,12 @@
 require "./character"
 
 class Hero < Character
-
+  def hero_attack
+    damage = @attack - monster.defence
+    monster.hp -= damage
+    puts <<~TEXT
+    勇者の攻撃！モンスターに#{damage}のダメージを与えた。"
+    モンスターのHPは残り#{monster.hp}だ！
+    TEXT
+  end
 end
--- monster.rb
+++ monster.rb
@@ -1,5 +1,11 @@
 require "./character"
-
 class Hero < Character
-
+  def monster_attack
+    damage = @attack - hero.defence
+    hero.hp -= damage
+    puts <<~TEXT
+    モンスターの攻撃！勇者は#{damage}のダメージを受けた。"
+    勇者のHPは残り#{hero.hp}だ！
+    TEXT
+  end
 end
